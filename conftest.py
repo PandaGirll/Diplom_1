e57a6337915c96c3011f0241d1a887fde51facb6
PandaGirll <nikaklimenko@gmail.com>
2024-07-24 01:00:08 +0300

    Созданы и обновлены вспомогательные файлы.

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ from unittest.mock import Mock
 from praktikum.burger import Burger
 from praktikum.bun import Bun
 from praktikum.ingredient import Ingredient
+from helpers import SAUCE_NAME, FILLING_NAME, SAUCE_PRICE, FILLING_PRICE, create_mock_ingredient
 
 
 @pytest.fixture
@@ -18,3 +19,13 @@ def mock_bun():
 @pytest.fixture
 def mock_ingredient():
     return Mock(spec=Ingredient)
+
+
+@pytest.fixture
+def mock_sauce():
+    return create_mock_ingredient(SAUCE_NAME, SAUCE_PRICE)
+
+
+@pytest.fixture
+def mock_filling():
+    return create_mock_ingredient(FILLING_NAME, FILLING_PRICE)
